1c26ccd60abc7bf9ba17b76a7940de98a72a2092
salman-abedin <salman104alvi@gmail.com>
2020-08-26 05:21:58 +0600

    debut

--- config.def.h
+++ config.def.h
@@ -3,9 +3,9 @@
 
 static int topbar = 1;      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 1;    /* -c option; centers dmenu on screen */
-static int min_width = 500; /* minimum width when centered */
+static int min_width = 100; /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {"monospace:size=15"};
+static const char *fonts[] = {"monospace:size=17"};
 static const char *prompt =
     NULL; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -24,4 +24,4 @@ static unsigned int lines = 10;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static const unsigned int border_width = 1;
+static const unsigned int border_width = 0;
