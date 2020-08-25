07003e3b2f1e132147cdf24c78e9f96abf3e98d8
salman-abedin <salman104alvi@gmail.com>
2020-08-26 05:56:48 +0600

    added a few patches

--- dmenu.c
+++ dmenu.c
@@ -27,7 +27,14 @@
 #define TEXTW(X) (drw_fontset_getwidth(drw, (X)) + lrpad)
 
 /* enums */
-enum { SchemeNorm, SchemeSel, SchemeOut, SchemeLast }; /* color schemes */
+enum {
+   SchemeNorm,
+   SchemeSel,
+   SchemeNormHighlight,
+   SchemeSelHighlight,
+   SchemeOut,
+   SchemeLast
+}; /* color schemes */
 
 struct item {
    char *text;
@@ -112,15 +119,47 @@ static char *cistrstr(const char *s, const char *sub) {
    return NULL;
 }
 
+static void drawhighlights(struct item *item, int x, int y, int maxw) {
+   int i, indent;
+   char *highlight;
+   char c;
+
+   if (!(strlen(item->text) && strlen(text))) return;
+
+   drw_setscheme(
+       drw, scheme[item == sel ? SchemeSelHighlight : SchemeNormHighlight]);
+   for (i = 0, highlight = item->text; *highlight && text[i];) {
+      if (*highlight == text[i]) {
+         /* get indentation */
+         c = *highlight;
+         *highlight = '\0';
+         indent = TEXTW(item->text);
+         *highlight = c;
+
+         /* highlight character */
+         c = highlight[1];
+         highlight[1] = '\0';
+         drw_text(drw, x + indent - (lrpad / 2), y,
+                  MIN(maxw - indent, TEXTW(highlight) - lrpad), bh, 0,
+                  highlight, 0);
+         highlight[1] = c;
+         i++;
+      }
+      highlight++;
+   }
+}
+
 static int drawitem(struct item *item, int x, int y, int w) {
+   int r;
    if (item == sel)
       drw_setscheme(drw, scheme[SchemeSel]);
    else if (item->out)
       drw_setscheme(drw, scheme[SchemeOut]);
    else
       drw_setscheme(drw, scheme[SchemeNorm]);
-
-   return drw_text(drw, x, y, w, bh, lrpad / 2, item->text, 0);
+   r = drw_text(drw, x, y, w, bh, lrpad / 2, item->text, 0);
+   drawhighlights(item, x, y, w);
+   return r;
 }
 
 static void drawmenu(void) {
@@ -775,7 +814,8 @@ static void setup(void) {
 static void usage(void) {
    fputs(
        "usage: dmenu [-bfiv] [-l lines] [-p prompt] [-fn font] [-m monitor]\n"
-       "             [-nb color] [-nf color] [-sb color] [-sf color] [-w "
+       "             [-nb color] [-nf color] [-sb color] [-sf color]\n"
+       "             [-nhb color] [-nhf color] [-shb color] [-shf color] [-w "
        "windowid]\n",
        stderr);
    exit(1);
@@ -820,6 +860,14 @@ int main(int argc, char *argv[]) {
          colors[SchemeSel][ColBg] = argv[++i];
       else if (!strcmp(argv[i], "-sf")) /* selected foreground color */
          colors[SchemeSel][ColFg] = argv[++i];
+      else if (!strcmp(argv[i], "-nhb")) /* normal hi background color */
+         colors[SchemeNormHighlight][ColBg] = argv[++i];
+      else if (!strcmp(argv[i], "-nhf")) /* normal hi foreground color */
+         colors[SchemeNormHighlight][ColFg] = argv[++i];
+      else if (!strcmp(argv[i], "-shb")) /* selected hi background color */
+         colors[SchemeSelHighlight][ColBg] = argv[++i];
+      else if (!strcmp(argv[i], "-shf")) /* selected hi foreground color */
+         colors[SchemeSelHighlight][ColFg] = argv[++i];
       else if (!strcmp(argv[i], "-w")) /* embedding window id */
          embed = argv[++i];
       else
