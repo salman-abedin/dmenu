1ce4a76a0abaac18be79478978532e99858b7419
salman-abedin <salman104alvi@gmail.com>
2020-08-27 10:26:52 +0600

    added a few patches

--- config.def.h
+++ config.def.h
@@ -6,7 +6,10 @@ static int fuzzy = 1;  /* -F  option; if 0, dmenu doesn't use fuzzy matching  */
 static int centered = 1;    /* -c option; centers dmenu on screen */
 static int min_width = 500; /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {"monospace:size=17"};
+static const char *fonts[] = {
+    "monospace:size=17",
+    "Noto Color Emoji:pixelsize=12:antialias=true:autohint=true",
+};
 static const char *prompt =
     NULL; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
