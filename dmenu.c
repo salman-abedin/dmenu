bc13181601d2656c67754f52600fc680cc71822a
salman-abedin <salman104alvi@gmail.com>
2020-08-28 15:36:19 +0600

    added a few patches

--- dmenu.c
+++ dmenu.c
@@ -317,11 +317,32 @@ static void grabkeyboard(void) {
    die("cannot grab keyboard");
 }
 
+static void readstdin(FILE *stream);
+
+static void refreshoptions() {
+   int dynlen = strlen(dynamic);
+   char *cmd = malloc(dynlen + strlen(text) + 2);
+   if (cmd == NULL) die("malloc:");
+   sprintf(cmd, "%s %s", dynamic, text);
+   FILE *stream = popen(cmd, "r");
+   if (!stream) die("popen(%s):", cmd);
+   readstdin(stream);
+   int pc = pclose(stream);
+   if (pc == -1) die("pclose:");
+   free(cmd);
+   curr = sel = items;
+}
+
 static void match(void) {
    if (fuzzy) {
       fuzzymatch();
       return;
    }
+
+   if (dynamic && *dynamic) {
+      refreshoptions();
+   }
+
    static char **tokv = NULL;
    static int tokn = 0;
 
@@ -342,7 +363,7 @@ static void match(void) {
    for (item = items; item && item->text; item++) {
       for (i = 0; i < tokc; i++)
          if (!fstrstr(item->text, tokv[i])) break;
-      if (i != tokc) /* not all tokens match */
+      if (i != tokc && !(dynamic && *dynamic)) /* not all tokens match */
          continue;
       /* exact matches go first, then prefixes, then substrings */
       if (!tokc || !fstrncmp(text, item->text, textsize))
@@ -642,13 +663,13 @@ static void paste(void) {
    drawmenu();
 }
 
-static void readstdin(void) {
+static void readstdin(FILE *stream) {
    char buf[sizeof text], *p;
    size_t i, imax = 0, size = 0;
    unsigned int tmpmax = 0;
 
    /* read each line from stdin and add it to the item list */
-   for (i = 0; fgets(buf, sizeof buf, stdin); i++) {
+   for (i = 0; fgets(buf, sizeof buf, stream); i++) {
       if (i + 1 >= size / sizeof *items)
          if (!(items = realloc(items, (size += BUFSIZ))))
             die("cannot realloc %u bytes:", size);
@@ -664,7 +685,7 @@ static void readstdin(void) {
    }
    if (items) items[i].text = NULL;
    inputw = items ? TEXTW(items[imax].text) : 0;
-   lines = MIN(lines, i);
+   if (!dynamic || !*dynamic) lines = MIN(lines, i);
 }
 
 static void run(void) {
@@ -814,9 +835,9 @@ static void setup(void) {
 static void usage(void) {
    fputs(
        "usage: dmenu [-bfiv] [-l lines] [-p prompt] [-fn font] [-m monitor]\n"
-       "             [-nb color] [-nf color] [-sb color] [-sf color]\n"
-       "             [-nhb color] [-nhf color] [-shb color] [-shf color] [-w "
-       "windowid]\n",
+       "             [-nb color] [-nf color] [-sb color] [-sf color] [-w "
+       "windowid]\n"
+       "[-dy command]\n",
        stderr);
    exit(1);
 }
@@ -870,6 +891,8 @@ int main(int argc, char *argv[]) {
          colors[SchemeSelHighlight][ColFg] = argv[++i];
       else if (!strcmp(argv[i], "-w")) /* embedding window id */
          embed = argv[++i];
+      else if (!strcmp(argv[i], "-dy")) /* dynamic command to run */
+         dynamic = argv[++i];
       else
          usage();
 
@@ -892,9 +915,9 @@ int main(int argc, char *argv[]) {
 
    if (fast && !isatty(0)) {
       grabkeyboard();
-      readstdin();
+      if (!(dynamic && *dynamic)) readstdin(stdin);
    } else {
-      readstdin();
+      if (!(dynamic && *dynamic)) readstdin(stdin);
       grabkeyboard();
    }
    setup();
